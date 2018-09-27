137e7f97efdac66916ebcba63115881b69616c3a
viktharien <viktharien@zoho.com>
2018-09-27 14:38:49 -0500

    add and test createalphabet method

--- gematria_sharp/Program.cs
+++ gematria_sharp/Program.cs
@@ -6,11 +6,38 @@ using System.Threading.Tasks;
 
 namespace gematria_sharp
 {
-    class Program
+    class Gematria
     {
+        public static Hashtable vals = new Hashtable();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateAlphabet();
+            foreach(DictionaryEntry entry in vals) {
+                Console.WriteLine(entry.Key + ":" + entry.Value);
+            }
+        }
+
+        static void CreateAlphabet() {
+            char[] arr = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            int[] nums = Enumerable.Range(1, 9).ToArray();
+            int count = 1;
+
+
+            foreach(char i in arr) {
+                vals.Add(i, count);
+                if (count < 9) {
+                    count += 1;
+                } else {
+                    count = 1;
+                }
+            }
+
+            //ICollection valKeys = vals.Keys;
+
+            //foreach(char i in valKeys) {
+            //    Console.WriteLine(i + ":" + vals[);
+            //}
+            //Console.ReadLine();
         }
     }
 }
