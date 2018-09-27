50ae18ee6ad2f8bc905faf9bf3e4e7417c7b6d73
viktharien <viktharien@zoho.com>
2018-09-27 14:09:41 -0500

    init

---
+++ gematria_sharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gematria_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
